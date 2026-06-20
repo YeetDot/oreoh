de8c41325f3b3ca81a157750abc503a476bcd918
rando <randomrepl@gmail.com>
2026-06-20 22:39:30 +0800

    Initial Commit

---
+++ src/client/java/com/pusillanimousfool/oreoh/client/OreOhClient.java
@@ -0,0 +1,9 @@
+package com.pusillanimousfool.oreoh.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreOhClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/client/java/com/pusillanimousfool/oreoh/client/OreOhDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pusillanimousfool.oreoh.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OreOhDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
